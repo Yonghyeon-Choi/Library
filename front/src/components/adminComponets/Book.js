00023f6a90f822fd78d4fc1e319c6b99a2b8e8b0
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-11 15:25:32 +0900

    1211

--- front/src/components/adminComponets/Book.js
+++ front/src/components/adminComponets/Book.js
@@ -7,6 +7,12 @@ const Book = (props) => {
     const initialBookState = {
         id: null,
         title: "",
+        isbn: "",
+        author: "",
+        publisher: "",
+        pubdate: "",
+        cnt: "",
+        brw: "",
         description: ""
     };
     const [currentBook, setCurrentBook] = useState(initialBookState);
@@ -82,7 +88,7 @@ const Book = (props) => {
                     <h4>Book</h4>
                     <form>
                         <div className="form-group">
-                            <label htmlFor="title">Title</label>
+                            <label htmlFor="title">제목</label>
                             <input
                                 type="text"
                                 className="form-control"
@@ -93,17 +99,109 @@ const Book = (props) => {
                             />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="description">Description</label>
+                            <label htmlFor="isbn">ISBN</label>
                             <input
                                 type="text"
+                                className="form-control"
+                                id="ISBN"
+                                name="ISBN"
+                                value={currentBook.isbn}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="author">저자</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="author"
+                                name="author"
+                                value={currentBook.author}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="publisher">출판사</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="publisher"
+                                name="publisher"
+                                value={currentBook.publisher}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="pubdate">출간일</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="pubdate"
+                                name="pubdate"
+                                value={currentBook.pubdate}
+                                onChange={handleInputChange}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="description">설명</label>
+                            <textarea
                                 className="form-control"
                                 id="description"
                                 name="description"
                                 value={currentBook.description}
                                 onChange={handleInputChange}
+                                rows={"5"}
                             />
                         </div>
                     </form>
+                    {/*<div>*/}
+                    {/*    <label>*/}
+                    {/*        <strong>제목</strong>*/}
+                    {/*    </label>{" "}*/}
+                    {/*    {currentBook.title}*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <label>*/}
+                    {/*        <strong>ISBN</strong>*/}
+                    {/*    </label>{" "}*/}
+                    {/*    {currentBook.isbn}*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <label>*/}
+                    {/*        <strong>저자</strong>*/}
+                    {/*    </label>{" "}*/}
+                    {/*    {currentBook.author}*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <label>*/}
+                    {/*        <strong>출판사</strong>*/}
+                    {/*    </label>{" "}*/}
+                    {/*    {currentBook.publisher}*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <label>*/}
+                    {/*        <strong>출간일</strong>*/}
+                    {/*    </label>{" "}*/}
+                    {/*    {currentBook.pubdate}*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <label>*/}
+                    {/*        <strong>설명</strong>*/}
+                    {/*    </label>{" "}*/}
+                    {/*    {currentBook.description}*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <label>*/}
+                    {/*        <strong>보유 수</strong>*/}
+                    {/*    </label>{" "}*/}
+                    {/*    {currentBook.cnt}*/}
+                    {/*</div>*/}
+                    {/*<div>*/}
+                    {/*    <label>*/}
+                    {/*        <strong>대출 중</strong>*/}
+                    {/*    </label>{" "}*/}
+                    {/*    {currentBook.brw}*/}
+                    {/*</div>*/}
 
                     <button className="badge badge-danger mr-2" onClick={removeBook}>
                         Delete

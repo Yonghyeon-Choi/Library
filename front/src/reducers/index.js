b8925eb8345149930fd3711ed7f4ec068f2e522e
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-12 02:05:41 +0900

    1212

--- front/src/reducers/index.js
+++ front/src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from "redux";
 import auth from "./auth";
 import message from "./message";
-import books from "./books";
 
 export default combineReducers({
   auth,
   message,
-  books,
 });

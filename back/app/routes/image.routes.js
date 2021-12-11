16ca5ae329555d32234c55b03f76492fc8551990
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-12 00:51:32 +0900

    1212

--- back/app/routes/image.routes.js
+++ back/app/routes/image.routes.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = express.Router();
 const controller = require("../controller/file.controller");
 
-let routes = (app) => {
+// let routes = (app) => {
+//     router.post("/upload", controller.upload);
+//     router.get("/list", controller.getListFiles);
+//
+//     app.use("/api/images", router);
+// };
+//
+// module.exports = routes;
+
+module.exports = app => {
+    const images = require("../controllers/file.controller.js");
+
+    var router = require("express").Router();
+
     router.post("/upload", controller.upload);
     router.get("/list", controller.getListFiles);
 
     app.use("/api/images", router);
 };
-
-module.exports = routes;

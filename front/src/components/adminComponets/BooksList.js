b21b4f62c21e89c52a40f52411bf246e180d3c9a
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-11 20:24:46 +0900

    1211

--- front/src/components/adminComponets/BooksList.js
+++ front/src/components/adminComponets/BooksList.js
@@ -102,20 +102,22 @@ const BooksList = (props) => {
                             </button>
                         </td>
                     </tr>
-                    // <div>
-                    //     <td/>
-                    //     <td><b>저자</b></td>
-                    //     <td/>
-                    //     <td>{book.author}</td>
-                    //     <td/>
-                    // </div>
-                    // <div>
-                    //     <td/>
-                    //     <td><b>출간일</b></td>
-                    //     <td/>
-                    //     <td>{book.pubdate}</td>
-                    //     <td/>
-                    // </div>
+                    <tr>
+                        <td></td>
+                        <td><b>저자</b></td>
+                        <td></td>
+                        <td>{book.author}</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td><b>출간일</b></td>
+                        <td></td>
+                        <td>{book.pubdate}</td>
+                        <td></td>
+                        <td></td>
+                    </tr>
                     ))}
 
                 </div>

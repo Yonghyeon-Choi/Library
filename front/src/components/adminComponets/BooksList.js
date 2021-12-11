2646f4b406747d61c9f7c1e4e272897349ce1aff
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-12 02:16:57 +0900

    1211

--- front/src/components/adminComponets/BooksList.js
+++ front/src/components/adminComponets/BooksList.js
@@ -74,100 +74,98 @@ const BooksList = (props) => {
     return (
         <div>
         {adminToken ? (
-                {/*<img src={images[0]['url']}/>*/}
-                <div className="card">
-                    <div style={{width: "100%"}}>{/*className="col-md-8"*/}
-                        <div className="input-group mb-3">
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="제목"
-                                value={searchTitle}
-                                onChange={onChangeSearchTitle}
-                                onKeyPress={onKeyPress}
-                            />
-                            <div className="input-group-append">
-                                <button
-                                    className="btn btn-outline-secondary"
-                                    type="button"
-                                    onClick={findByTitle}
-                                >
-                                    Search
-                                </button>
-                            </div>
+            <div className="card">
+                <div style={{width: "100%"}}>{/*className="col-md-8"*/}
+                    <div className="input-group mb-3">
+                        <input
+                            type="text"
+                            className="form-control"
+                            placeholder="제목"
+                            value={searchTitle}
+                            onChange={onChangeSearchTitle}
+                            onKeyPress={onKeyPress}
+                        />
+                        <div className="input-group-append">
+                            <button
+                                className="btn btn-outline-secondary"
+                                type="button"
+                                onClick={findByTitle}
+                            >
+                                Search
+                            </button>
                         </div>
                     </div>
-                    <div style={{width: "100%"}}>
-                        <table width={"100%"}>
-                            <tbody>
-                            <tr>
-                                <td width={"87%"}>
-                                    <h5>책 목록</h5>
-                                </td>
-                                <td width={"13%"} className={"right-align"}>
-                                    <button
-                                        className="addBtnStyle"
-                                        type="button"
-                                        onClick={addBook}
-                                    >
-                                        등록
-                                    </button>
-                                </td>
-                            </tr>
-                            </tbody>
-                        </table>
-                        <hr/>
-
-                        {books && books.map((book, index) => (
-                            <div key={index}>
-                                <table width={"100%"} style={{fontSize: "11px"}}>
-                                    <tbody>
-                                    <tr>
-                                        <td width={"15%"}>book cover</td>
-                                        <td width={"2%"}/>
-                                        <td width={"10%"} className={"right-align"}><b>제목</b></td>
-                                        <td width={"1%"}/>
-                                        <td width={"57%"}>{book.title}</td>
-                                        <td width={"2%"}/>
-                                        <td width={"13%"}  className={"right-align"}>
-                                            <button
-                                                type="button"
-                                                className="editBtnStyle"
-                                                onClick={() => openBook(index)}>
-                                                관리
-                                            </button>
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td></td>
-                                        <td></td>
-                                        <td className={"right-align"}><b>저자</b></td>
-                                        <td></td>
-                                        <td>{book.author}</td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                    <tr>
-                                        <td></td>
-                                        <td></td>
-                                        <td className={"right-align"}><b>출간</b></td>
-                                        <td></td>
-                                        <td>{book.publisher} {book.pubdate}</td>
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                    </tbody>
-                                </table>
-                                <br/>
-                            </div>
-                        ))}
-
-                    </div>
                 </div>
-            ):(
-                props.history.push("/login")
-            )
-        }
+                <div style={{width: "100%"}}>
+                    <table width={"100%"}>
+                        <tbody>
+                        <tr>
+                            <td width={"87%"}>
+                                <h5>책 목록</h5>
+                            </td>
+                            <td width={"13%"} className={"right-align"}>
+                                <button
+                                    className="addBtnStyle"
+                                    type="button"
+                                    onClick={addBook}
+                                >
+                                    등록
+                                </button>
+                            </td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <hr/>
+
+                    {books && books.map((book, index) => (
+                        <div key={index}>
+                            <table width={"100%"} style={{fontSize: "11px"}}>
+                                <tbody>
+                                <tr>
+                                    <td width={"15%"}>book cover</td>
+                                    <td width={"2%"}/>
+                                    <td width={"10%"} className={"right-align"}><b>제목</b></td>
+                                    <td width={"1%"}/>
+                                    <td width={"57%"}>{book.title}</td>
+                                    <td width={"2%"}/>
+                                    <td width={"13%"}  className={"right-align"}>
+                                        <button
+                                            type="button"
+                                            className="editBtnStyle"
+                                            onClick={() => openBook(index)}>
+                                            관리
+                                        </button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td className={"right-align"}><b>저자</b></td>
+                                    <td></td>
+                                    <td>{book.author}</td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td className={"right-align"}><b>출간</b></td>
+                                    <td></td>
+                                    <td>{book.publisher} {book.pubdate}</td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                                </tbody>
+                            </table>
+                            <br/>
+                        </div>
+                    ))}
+
+                </div>
+            </div>
+        ):(
+            props.history.push("/login")
+        )}
         </div>
     );
 };

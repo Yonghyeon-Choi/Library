db7898d89244ffe2227d4c99ee3e73351d0e935f
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-12 02:15:22 +0900

    1211

--- front/src/components/adminComponets/BooksList.js
+++ front/src/components/adminComponets/BooksList.js
@@ -74,7 +74,7 @@ const BooksList = (props) => {
     return (
         <div>
         {images && adminToken ? (
-                {typeof(images[0])}
+                {typeof(images)}
                 {/*<img src={images[0]['url']}/>*/}
                 <div className="card">
                     <div style={{width: "100%"}}>{/*className="col-md-8"*/}

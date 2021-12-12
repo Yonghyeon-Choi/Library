8f74dd0c52176ea004de7278dd7fe7e279e9540e
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-12 20:05:38 +0900

    1212

--- front/src/components/adminComponets/BooksList.js
+++ front/src/components/adminComponets/BooksList.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import { Link } from "react-router-dom";
 import "../GlobalStyles.css";
 
 import bookService from "../../services/book.service";

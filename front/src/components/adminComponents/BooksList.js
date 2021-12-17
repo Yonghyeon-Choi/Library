2479fbc39d663cb22a859f3b6d58ef903bfc955a
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-18 01:08:28 +0900

    1217

--- front/src/components/adminComponents/BooksList.js
+++ front/src/components/adminComponents/BooksList.js
@@ -65,7 +65,7 @@ const BooksList = (props) => {
     };
 
     const onKeyPress = (e) => {
-        e.preventDefault();
+        // e.preventDefault();
         if(e.key === "Enter") findByTitle();
     };
 

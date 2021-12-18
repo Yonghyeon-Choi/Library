10a079cbcf7ab4b88bf040034dd6944d0722be99
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-19 01:19:29 +0900

    1217

--- front/src/components/userComponents/Book.js
+++ front/src/components/userComponents/Book.js
@@ -134,8 +134,8 @@ const Book = (props) => {
 
     return (
         <div>
+            {typeof(user)}
             {Object.values(user)}
-            {userid}
             {currentBook ? (
                 <div className="edit-form">
                     <h5>도서 정보</h5>

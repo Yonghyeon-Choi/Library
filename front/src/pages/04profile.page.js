2abc5f082e384fa0ad936efa7abc74f9c2da3f44
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-19 03:03:06 +0900

    1219

--- front/src/pages/04profile.page.js
+++ front/src/pages/04profile.page.js
@@ -3,50 +3,8 @@ import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
 import bookService from "../services/book.service";
 
-const [books, setBooks] = useState([]);
-
-const retrieveBooks = () => {
-    bookService.getAll()
-        .then(response => {
-            setBooks(response.data);
-            // console.log(response.data);
-        })
-        .catch(e => {
-            console.log(e);
-        });
-};
-
-useEffect(() => {
-    retrieveBooks();
-}, []);
-
-const getBook = (id) => {
-    let title = "";
-    for(let i = 0; i < books.length; i++){
-        if(books[i].id === id){
-            title = books[i].title;
-            break
-        }
-    }
-    return title
-};
-
-const KST = (utc) => {
-    const KST = new Date(utc);
-
-    let year = KST.getFullYear();
-    let month = ('0' + (KST.getMonth() + 1)).slice(-2);
-    let day = ('0' + KST.getDate()).slice(-2);
-    let dateString = year + '년' + month  + '월' + day +"일 ";
-    let hours = ('0' + KST.getHours()).slice(-2);
-    let minutes = ('0' + KST.getMinutes()).slice(-2);
-    let seconds = ('0' + KST.getSeconds()).slice(-2);
-    let timeString = hours + '시' + minutes  + '분' + seconds + "초";
-
-    return dateString + timeString;
-};
-
 class Profile extends Component {
+
     render() {
         const { user: currentUser } = this.props;
 
@@ -118,6 +76,48 @@ class Profile extends Component {
 
 function mapStateToProps(state) {
     const { user } = state.auth;
+    const [books, setBooks] = useState([]);
+
+    const retrieveBooks = () => {
+        bookService.getAll()
+            .then(response => {
+                setBooks(response.data);
+                // console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+    };
+
+    useEffect(() => {
+        retrieveBooks();
+    }, []);
+
+    const getBook = (id) => {
+        let title = "";
+        for(let i = 0; i < books.length; i++){
+            if(books[i].id === id){
+                title = books[i].title;
+                break
+            }
+        }
+        return title
+    };
+
+    const KST = (utc) => {
+        const KST = new Date(utc);
+
+        let year = KST.getFullYear();
+        let month = ('0' + (KST.getMonth() + 1)).slice(-2);
+        let day = ('0' + KST.getDate()).slice(-2);
+        let dateString = year + '년' + month  + '월' + day +"일 ";
+        let hours = ('0' + KST.getHours()).slice(-2);
+        let minutes = ('0' + KST.getMinutes()).slice(-2);
+        let seconds = ('0' + KST.getSeconds()).slice(-2);
+        let timeString = hours + '시' + minutes  + '분' + seconds + "초";
+
+        return dateString + timeString;
+    };
     return {
         user,
     };

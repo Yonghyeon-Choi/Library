a70518ef370cc5be6286e0cf2a21f62bb4d5ecee
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-19 06:20:17 +0900

    1219

--- front/src/components/usermanageComponents/BorrowBookList.js
+++ front/src/components/usermanageComponents/BorrowBookList.js
@@ -126,34 +126,34 @@ const BorrowBookList = (props) => {
 
         console.log(currentBorrow.brws);
 
-        // let userdata = {
-        //     id: user.id,
-        //     brws: user.brws,
-        // };
-        //
-        // usermanageService.borrow(userid, userdata)
-        //     .then(response => {
-        //         console.log(response.data);
-        //     })
-        //     .catch(e => {
-        //         console.log(e);
-        //     });
-        //
-        // let bookdata = {
-        //     id: currentBorrow.id,
-        //     brw: currentBorrow.brw - 1,
-        //     brws: currentBorrow.brws,
-        // };
-        //
-        // bookService.borrow(currentBorrow.id, bookdata)
-        //     .then(response => {
-        //         console.log(response.data);
-        //     })
-        //     .catch(e => {
-        //         console.log(e);
-        //     });
+        let userdata = {
+            id: user.id,
+            brws: user.brws,
+        };
 
-        // window.location.reload();
+        usermanageService.borrow(userid, userdata)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+
+        let bookdata = {
+            id: currentBorrow.id,
+            brw: currentBorrow.brw - 1,
+            brws: currentBorrow.brws,
+        };
+
+        bookService.borrow(currentBorrow.id, bookdata)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(e => {
+                console.log(e);
+            });
+
+        window.location.reload();
     };
 
     return (

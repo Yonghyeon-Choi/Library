54827e7a0fe5fd7f3fb3bbeed1f56ca2de16ae15
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-19 01:17:41 +0900

    1217

--- front/src/components/userComponents/Book.js
+++ front/src/components/userComponents/Book.js
@@ -5,7 +5,7 @@ import usermanageService from "../../services/usermanage.service";
 import "../GlobalStyles.css";
 
 const Book = (props) => {
-    const user = window.localStorage.getItem('user').entries;
+    const user = window.localStorage.getItem('user');
     const userid = user.id;
     const initialBookState = {
         id: null,

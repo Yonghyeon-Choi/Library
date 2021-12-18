4c914b0a5d5d90fda81668c43e1378d310c0c2a2
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-19 02:57:46 +0900

    1219

--- front/src/pages/04profile.page.js
+++ front/src/pages/04profile.page.js
@@ -1,10 +1,54 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Redirect } from "react-router-dom";
+import bookService from "../../services/book.service";
 
 class Profile extends Component {
     render() {
         const { user: currentUser } = this.props;
+
+        const [books, setBooks] = useState([]);
+
+        const retrieveBooks = () => {
+            bookService.getAll()
+                .then(response => {
+                    setBooks(response.data);
+                    // console.log(response.data);
+                })
+                .catch(e => {
+                    console.log(e);
+                });
+        };
+
+        useEffect(() => {
+            retrieveBooks();
+        }, []);
+
+        const getBook = (id) => {
+            let title = "";
+            for(let i = 0; i < books.length; i++){
+                if(books[i].id === id){
+                    title = books[i].title;
+                    break
+                }
+            }
+            return title
+        };
+
+        const KST = (utc) => {
+            const KST = new Date(utc);
+
+            let year = KST.getFullYear();
+            let month = ('0' + (KST.getMonth() + 1)).slice(-2);
+            let day = ('0' + KST.getDate()).slice(-2);
+            let dateString = year + '년' + month  + '월' + day +"일 ";
+            let hours = ('0' + KST.getHours()).slice(-2);
+            let minutes = ('0' + KST.getMinutes()).slice(-2);
+            let seconds = ('0' + KST.getSeconds()).slice(-2);
+            let timeString = hours + '시' + minutes  + '분' + seconds + "초";
+
+            return dateString + timeString;
+        };
 
         return (
             <div
@@ -21,7 +65,7 @@ class Profile extends Component {
                             <tr>
                                 <th colSpan="3">
                                     <h3>
-                                        <strong>{currentUser.username}'s Profile</strong>
+                                        <strong>{currentUser.username} 마이페이지</strong>
                                     </h3>
                                 </th>
                             </tr>
@@ -38,12 +82,6 @@ class Profile extends Component {
                             </tr>
                             <tr>
                                 <td>
-                                    <strong>DB ID </strong>
-                                </td>
-                                <td>{currentUser.id}</td>
-                            </tr>
-                            <tr>
-                                <td>
                                     <strong>Name </strong>
                                 </td>
                                 <td>{currentUser.username}</td>
@@ -56,9 +94,15 @@ class Profile extends Component {
                             </tr>
                             <tr>
                                 <td>
-                                    <strong>Authorities </strong>
+                                    <strong>대출 도서 </strong>
                                 </td>
-                                <td>{currentUser.roles[0]}</td>
+                                <td>{currentUser.brws.map((book, index)=>(
+                                    <div key={index}>
+                                        <b>도서명</b>&nbsp;{getBook(book.bookid)}<br/>
+                                        <b>대출일</b>&nbsp;{KST(book.brwtime)}<br/><br/>
+                                    </div>
+                                ))}
+                                </td>
                             </tr>
                             </tbody>
                         </table>

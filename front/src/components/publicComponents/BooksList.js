2edd689ab60b9a5ebd58663b5a8e01850b452541
Yonghyeon-Choi <choiyh0601@naver.com>
2021-12-19 06:28:40 +0900

    1219

--- front/src/components/publicComponents/BooksList.js
+++ front/src/components/publicComponents/BooksList.js
@@ -119,9 +119,9 @@ const BooksList = (props) => {
                             <tbody>
                             <tr>
                                 <td width={"15%"} rowSpan={5}>{imageView(book)}</td>
-                                <td width={"2%"}/>
+                                <td width={"5%"}/>
                                 <td width={"10%"} className={"right-align"}><b>제목</b></td>
-                                <td width={"1%"}/>
+                                <td width={"5%"}/>
                                 <td width={"65%"}><b>{book.title}</b></td>
                             </tr>
                             <tr>
